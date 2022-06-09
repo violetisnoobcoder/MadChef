624e7cd29a06b50f72205aebc7cbebd0611afc94
NKC27 <93439579+NKC27@users.noreply.github.com>
2022-06-09 15:53:23 +0100

    App updated with DND provider

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from "react";
+
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+
 import "./App.css";
+import Plate from "./components/plate/Plate";
 import Home from "./pages/home/Home";
 
 const App = () => {
   return (
-    <div>
-      <Home />
-    </div>
+    <DndProvider backend={HTML5Backend}>
+      <div>
+        <Home />
+      </div>
+
+      <div className="App">
+        <Plate />
+      </div>
+    </DndProvider>
   );
 };
 
