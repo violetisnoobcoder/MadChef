f6b269779766d0b05aeda977e5ca765e7c1e6ada
sikandersultan <sikandersultanmirza@gmail.com>
2022-06-10 20:28:06 +0100

    Logo click now returns home

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -14,7 +14,7 @@ const AppNavbar = () => {
     <>
       <Navbar bg='dark' variant='dark' expand='lg'>
         <Container fluid>
-          <Navbar.Brand as={Link} to='/search'>
+          <Navbar.Brand as={Link} to='/'>
             MadChef
           </Navbar.Brand>
           <Navbar.Toggle aria-controls='navbar' />
