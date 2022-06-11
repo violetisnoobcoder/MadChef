b7f55a33e61a9cf3b80c7f79436d9a94c2eee023
NKC27 <93439579+NKC27@users.noreply.github.com>
2022-06-11 10:21:58 +0100

    style changes updated

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,21 @@
 import Splash from "../components/splash/Splash";
 import "./home.scss";
-import React from 'react';
-import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
-import Auth from '../utils/auth';
+import React from "react";
+import {
+  Jumbotron,
+  Container,
+  CardColumns,
+  Card,
+  Button,
+  Navbar,
+} from "react-bootstrap";
+import Auth from "../utils/auth";
+import Picture from "../components/picture/Picture";
 
 const Home = () => {
   return (
     <div className="home">
+      <Navbar />
       <Splash />
     </div>
   );
