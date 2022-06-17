5eef9674e245e5df88bab8aab3d69b7281e717e9
roconn <roconn25@gmail.com>
2022-06-17 21:47:43 +0100

    recipebuilder page styled

--- client/src/components/RecipeBuilder/RecipeBuilder.js
+++ client/src/components/RecipeBuilder/RecipeBuilder.js
@@ -49,7 +49,7 @@ function App() {
       updatedList = [...checked, event.target.value];
       try {
         const response = await fetch(
-          `https://api.spoonacular.com/recipes/findByIngredients?apiKey=b622f7d1fa414549a865f90abc479acf&ingredients=${updatedList}&number=3`
+          `https://api.spoonacular.com/recipes/findByIngredients?apiKey=b622f7d1fa414549a865f90abc479acf&ingredients=${updatedList}&number=9`
         );
         if (!response.ok) {
           throw new Error("Something went wrong!");
@@ -107,39 +107,66 @@ function App() {
   var isChecked = (item) =>
     checked.includes(item) ? "checked-item" : "not-checked-item";
   return (
+
+
+
+
+    
     <div className="app">
       <div className="imgReset">
         <img
-          className="recpieSplashScreen"
-          src="https://images.unsplash.com/photo-1606787366850-de6330128bfc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+          className="recipeSplashScreen"
+          // src="https://images.unsplash.com/photo-1606787366850-de6330128bfc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+          src="images/custom-splash.png"
         />
         <div className="centered">
           <div className="slogan">
             <span className="sloganText1">
-              Lets Get Started & Select Your Ingredients!
+              COMBINE YOUR INGREDIENTS BELOW TO GENERATE A MAD RECIPE
             </span>
           </div>
         </div>
       </div>
+      {/* HERE */}
+      <div className="title">Ingredients to build with:</div>
+        <div className="list-container">
+          {checkList.map((item, index) => (
+            <div className="check-list" key={index}>
+              <input value={item.item} type="checkbox" onChange={handleCheck} />
+              <span className={isChecked(item.item)}>
+                <img
+                  className="ingredients"
+                  src={process.env.PUBLIC_URL + item.image}
+                ></img>
+              </span>
+            </div>
+          ))}
+        </div>
       <div className="checkList">
         <div>
+        <h2 className="results">
+              {searchedRecipes.length
+                ? `Viewing ${searchedRecipes.length} results:`
+                : "Click your ingredients to generate a recipe!"}
+            </h2>
           <Container>
-            <h2>
+            {/* <h2 className="results">
               {searchedRecipes.length
                 ? `Viewing ${searchedRecipes.length} results:`
                 : "Search for a recipe to begin"}
-            </h2>
+            </h2> */}
             <CardColumns>
               {searchedRecipes.map((data) => {
                 return (
-                  <Card key={data.recipeId} style={{ width: "18rem" }}>
+                  <Card className="recipe-card" key={data.recipeId} style={{ width: "18rem" }}>
                     <Card.Img variant="top" src={data.image} />
                     <Card.Body>
                       <Card.Title className="font-family: 'Anton';">
                         {data.title}
                       </Card.Title>
                       <Button
-                        variant="primary"
+                      className="see-more-btn"
+                        variant="dark"
                         id={data.recipeId}
                         onClick={showDetails}
                       >
@@ -166,7 +193,7 @@ function App() {
             </CardColumns>
           </Container>
         </div>
-        <div className="title">Your CheckList:</div>
+        {/* <div className="title">Your CheckList:</div>
         <div className="list-container">
           {checkList.map((item, index) => (
             <div className="check-list" key={index}>
@@ -179,7 +206,7 @@ function App() {
               </span>
             </div>
           ))}
-        </div>
+        </div> */}
       </div>
     </div>
   );
